710772a91632640572fb8af94d73059e00f776c5
kalen <jkalend@gmail.com>
2023-03-07 19:08:41 +0100

    added parser tests

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ import re
 from dataclasses import dataclass
 from typing import Generator, Dict, Callable, List, Tuple, TextIO
 from xml.etree.ElementTree import Element
-from interpret import XMLParser, ArgumentParser
+from interpret import XMLParser, ArgumentParser, Instruction
 
-xml = XMLParser().root
-for i in xml:
-    print(i.tag)
+xml = XMLParser()
 
-
-#print(xml.attrib)
+instructions = [Instruction(i) for i in xml.get_instructions()]
+instructions.sort(key=lambda x: x.index)
+for i in instructions:
+    print(i.index + 1)
