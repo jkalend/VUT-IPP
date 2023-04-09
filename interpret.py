ba0cdd1f72f9d9dd041a92d42b14234b7c91dff2
kalen <jkalend@gmail.com>
2023-04-09 02:23:09 +0200

    added exits with invalid args

--- interpret.py
+++ interpret.py
@@ -14,11 +14,10 @@ class ArgumentParser:
         self.parser = argparse.ArgumentParser(prog='Interpret XML',
                                               description='Interprets XML code made by parse.php'
                                                           ' and outputs the result to stdout',
-                                              epilog='At least one of the arguments --source or --input must be present'
-                                                     ' (Jan Kalenda 2023)'
-                                              )
+                                              epilog='(Jan Kalenda 2023)', add_help=False)
         self.parser.add_argument('--source', nargs="?", type=str, help='Source file')
         self.parser.add_argument('--input', nargs="?", type=str, help='Input file')
+        self.parser.add_argument('--help', action="store_true", help='Prints this help')
 
     def parse(self) -> argparse.Namespace:
         """Parses arguments from command line
@@ -26,8 +25,17 @@ class ArgumentParser:
         :return: parsed arguments
         """
         args = self.parser.parse_args()
+        if args.help and args.source is None and args.input is None:
+            self.parser.print_help()
+            sys.exit(0)
+        elif args.help and (args.source is not None or args.input is not None):
+            print("Error: Argument --help cannot be used with any other argument", file=sys.stderr)
+            sys.exit(10)
+
         if args.source is None and args.input is None:
-            self.parser.error('At least one of the arguments --source or --input must be present')
+            self.parser.print_help()
+            print("\nError: At least one of the arguments --source or --input must be present", file=sys.stderr)
+            sys.exit(10)
         return args
 
 
@@ -53,9 +61,12 @@ class XMLParser:
 
         try:
             return ET.parse(self.args.source).getroot()
-        except (ET.ParseError, FileNotFoundError):
+        except ET.ParseError:
             print("Error: XML file is not valid", file=sys.stderr)
             sys.exit(31)
+        except FileNotFoundError:
+            print("Error: XML file not found", file=sys.stderr)
+            sys.exit(11)
 
     @staticmethod
     def _load_xml() -> str:
